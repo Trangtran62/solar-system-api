e6f3490cbd54aad80afb4058420429e2f43daab9
Trang Tran <trantrang181197@gmail.com>
2023-04-20 11:55:15 -0700

    set up flask and created READ endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, order_from_sun):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.order_from_sun = order_from_sun
+
+planets = [
+    planet1 = Planet(1, "Earth", "weird", 3), 
+    planet2 = Planet(2, "Mars", "hot", 4), 
+    planet3 = Planet(3, "Venus", "orange", 5)
+    ]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def get_planets():
+    response = []
+    for planet in planets:
+        planet_dict = {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description, 
+        "order_from_sun": planet.order_from_sun }
+
+        response.append(planet_dict)
+
+    return jsonify(response), 200
