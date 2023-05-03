998b130a3d62f9d824eb98585d72c34a040fe909
Trang Tran <trantrang181197@gmail.com>
2023-05-03 12:16:21 -0700

    commit before pull

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app(test_config=None):
+def create_app(testing=False):
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ def add_planet():
     db.session.add(new_planet)
     db.session.commit()
     
-    return make_response(f"Planet {new_planet} successfuly created", 201)
+    return make_response({"message": f"Planet {new_planet} successfuly created"}, 201)
 
 @planet_bp.route("", methods=["GET"]) 
 def get_planets():
@@ -31,13 +31,12 @@ def validate_id(planet_id):
     except:
         abort(make_response({"message": f"planet {planet_id} invalid"}, 400))
     planet = Planet.query.get_or_404(planet_id)
-    
     return planet
 
 @planet_bp.route("/<planet_id>", methods=["GET"]) 
 def get_one_planet(planet_id):
     planet = validate_id(planet_id)
-    return planet.to_dict()
+    return jsonify(planet.to_dict())
 
 @planet_bp.route("/<planet_id>", methods=["PUT"]) 
 def update_one_planet(planet_id):
@@ -48,13 +47,12 @@ def update_one_planet(planet_id):
     planet.order_from_sun = request_body["order_from_sun"]
     
     db.session.commit()
-    return {"message": f"planet {planet_id} updated"}
+    return make_response({"message": f"planet {planet_id} updated"}, 200)
 
 @planet_bp.route("/<planet_id>", methods=["DELETE"])
 def delete_one_planet(planet_id):
     planet = validate_id(planet_id)
     db.session.delete(planet)
     db.session.commit()
-    return {"message": f"planet {planet_id} deleted"}
-
+    return make_response({"message": f"planet {planet_id} deleted"}, 200)
 
